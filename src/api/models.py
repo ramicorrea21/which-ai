f11410e8728f6690988e04c508f670668ee43320
Ramiro Correa <98496459+ramicorrea21@users.noreply.github.com>
2024-01-02 20:14:10 +0000

    first commit

--- src/api/models.py
+++ src/api/models.py
@@ -9,11 +9,29 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Tool(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False, unique=False)
+    description = db.Column(db.Text, nullable=False, unique=False)
+    creator = db.Column(db.String(250), nullable=False, unique=False)
+    category = db.Column(db.String(250), nullable=False, unique=False)
+    website = db.Column(db.Text, nullable=False, unique=False)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "creator": self.creator,
+            "category": self.category,
+            "website": self.website
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,21 +2,103 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Tool
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+import os 
+import json
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+tool_path = os.path.join(os.path.dirname(__file__), "tools.json")
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route("/populate", methods=["GET"])
+def personal_population():
+    with open(tool_path, "r") as file:
+        data = json.load(file)
+        file.close
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+        for tool in data:
+            tool = Tool(
+                name=tool["name"],
+                category=tool["category"],
+                creator=tool["creator"],
+                website=tool["website"],
+                description=tool["description"],
+            )
+            db.session.add(tool)
+            try:
+                db.session.commit()
+            except Exception as error:
+                print("error:", error.args)
+                return jsonify("rodo fallo"), 500
+        
+    return jsonify("todo funciono"), 201
+        
+#get all tools
+@api.route('/tools', methods=['GET'])
+def get_tools():
+    tools = Tool.query.all()
+    return jsonify(list(map(lambda tool : tool.serialize(), tools))), 200
 
-    return jsonify(response_body), 200
+#get tool by id
+@api.route('/tools/<int:id>', methods=['GET'])
+def get_tool(id):
+    tool = Tool.query.filter_by(id=id).one_or_none()
+    if tool:
+        return jsonify(tool.serialize()), 200
+    else: 
+        return jsonify({"error":"no tool found"}), 400
+
+#post tool
+@api.route('/addtool', methods=['POST'])
+def addtool():
+    body = request.json
+    name = body.get("name")
+    creator = body.get("creator")
+    description = body.get("description")
+    website = body.get("website")
+    category = body.get("category")
+
+    if name is None or creator is None or description is None or website is None or category is None:
+        return jsonify({"error":"bad request"}), 400
+    
+    tool = Tool(name=name, creator=creator, description=description, website=website, category=category)
+    db.session.add(tool)
+
+    try:
+        db.session.commit()
+        return jsonify({"message":"tool added"}), 201
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"error":f"{error}"})
+
+#edit tool
+@api.route('/edit_tool/<int:id>', methods=['PUT'])
+def edittool(id):
+    body = request.json
+    name = body.get("name")
+    creator = body.get("creator")
+    description = body.get("description")
+    website = body.get("website")
+    category = body.get("category")
+    tool = Tool.query.filter_by(id=id).one_or_none()
+
+    if name is None or creator is None or description is None or website is None or category is None:
+        return jsonify({"error":"bad request"}), 400
+    
+    tool.name = name
+    tool.creator = creator
+    tool.description = description
+    tool.website = website
+    tool.category = category
+
+    try:
+        db.session.commit()
+        return jsonify({"messaage":"tool edited"})
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"error":f"{error}"})
